295e8ea05b2bdab95aa35aa13e1edbe5c1568624
saigopal <saigopalpekal@gmail.com>
2020-11-22 21:10:12 +0530

    04-Writing Unit Test for findById method

---
+++ src/test/java/in/psg/jpa/repository/CourseRepositoryTest.java
@@ -0,0 +1,35 @@
+package in.psg.jpa.repository;
+
+import in.psg.jpa.SpringJpaHibernateDemoApplication;
+import in.psg.jpa.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = SpringJpaHibernateDemoApplication.class)
+public class CourseRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Test
+    public void contextLoads() {
+        logger.info("Test is Running...");
+    }
+
+    @Test
+    public void findById_basic(){
+        Course course = courseRepository.findById(10001L);
+        assertEquals("JPA Course in 50 Steps",course.getName());
+    }
+
+
+
+}
